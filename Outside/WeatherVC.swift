821bc37eb8db8907e0f6e53558b54d8c90cad022
Eugene Pavlov <eugenepavlov@Eugenes-MacBook-Pro.local>
2016-12-22 11:56:57 -0500

    Build weather UI for main screen

---
+++ Outside/WeatherVC.swift
@@ -0,0 +1,46 @@
+//
+//  WeatherVC.swift
+//  Outside
+//
+//  Created by Eugene Pavlov on 12/20/16.
+//  Copyright © 2016 Eugene Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var currentTempLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // todo
+    }
+}
+
